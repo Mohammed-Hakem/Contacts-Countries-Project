f268dfc8ad5f3a463d9a59fc3f298eeeb4809d53
Mohammed <mkkanpp0@gmail.com>
2026-03-10 23:00:05 +0300

    Initial commit: Added 3-Tier Architecture layers

---
+++ BusinessLayer/BusinessL.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Data;
+using System.Runtime.CompilerServices;
+using ContactsDataAccessLayer;
+
+
+namespace ContactsBusinessLayer
+{
+    public class clsContact
+    {
+
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public int ID { set; get; }
+        public string FirstName { set; get; }
+        public string LastName { set; get; }
+        public string Email { set; get; }
+        public string Phone { set; get; }
+        public string Address { set; get; }
+        public DateTime DateOfBirth { set; get; }
+
+        public string ImagePath { set; get; }
+
+        public int CountryID { set; get; }
+
+        public clsContact()
+
+        {
+            this.ID = -1;
+            this.FirstName = "";
+            this.LastName = "";
+            this.Email = "";
+            this.Phone = "";
+            this.Address = "";
+            this.DateOfBirth = DateTime.Now;
+            this.CountryID = -1;
+            this.ImagePath = "";
+
+            Mode = enMode.AddNew;
+
+        }
+
+        private clsContact(int ID, string FirstName, string LastName,
+            string Email, string Phone, string Address, DateTime DateOfBirth, int CountryID, string ImagePath)
+
+        {
+            this.ID = ID;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Email = Email;
+            this.Phone = Phone;
+            this.Address = Address;
+            this.DateOfBirth = DateOfBirth;
+            this.CountryID = CountryID;
+            this.ImagePath = ImagePath;
+
+            Mode = enMode.Update;
+
+        }
+
+        private bool _AddNewContact()
+        {
+            //call DataAccess Layer 
+
+            this.ID = clsContactDataAccess.AddNewContact(this.FirstName, this.LastName, this.Email, this.Phone,
+                this.Address, this.DateOfBirth, this.CountryID, this.ImagePath);
+
+            return (this.ID != -1);
+        }
+
+        private bool _UpdateContact()
+        {
+            //call DataAccess Layer 
+
+            return clsContactDataAccess.UpdateContact(this.ID, this.FirstName, this.LastName, this.Email, this.Phone,
+                 this.Address, this.DateOfBirth, this.CountryID, this.ImagePath);
+
+        }
+
+        public static clsContact Find(int ID)
+        {
+
+            string FirstName = "", LastName = "", Email = "", Phone = "", Address = "", ImagePath = "";
+            DateTime DateOfBirth = DateTime.Now;
+            int CountryID = -1;
+
+            if (clsContactDataAccess.GetContactInfoByID(ID, ref FirstName, ref LastName,
+                          ref Email, ref Phone, ref Address, ref DateOfBirth, ref CountryID, ref ImagePath))
+
+                return new clsContact(ID, FirstName, LastName,
+                           Email, Phone, Address, DateOfBirth, CountryID, ImagePath);
+            else
+                return null;
+
+        }
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNewContact())
+                    {
+
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case enMode.Update:
+
+                    return _UpdateContact();
+
+            }
+
+
+
+
+            return false;
+        }
+
+        public static DataTable GetAllContacts()
+        {
+            return clsContactDataAccess.GetAllContacts();
+
+        }
+
+        public static bool DeleteContact(int ID)
+        {
+            return clsContactDataAccess.DeleteContact(ID);
+        }
+
+        public static bool isContactExist(int ID)
+        {
+            return clsContactDataAccess.IsContactExist(ID);
+        }
+
+
+    }
+
+
+
+    //===========================================================================
+    //============================[ Countries Class ]============================
+    //===========================================================================
+
+    public class clsCountry
+    {
+        public enum enMode { AddNew = 0, Update = 1 }
+        public enMode Mode = enMode.AddNew;
+
+
+        public int CountryID { get; set; }
+        public string CountryName { get; set; }
+
+        public string Code { get; set; }
+
+        public string PhoneCode { get; set; }
+
+        public clsCountry()
+        {
+            CountryID = 0;
+            CountryName = "";
+            Code = "";
+            PhoneCode = "";
+        }
+
+        private clsCountry(int CountryID , string CountryName, string Code ,String PhoneCode)
+        {
+            this.CountryName = CountryName;
+            this.CountryID = CountryID;
+            this.Code = Code;
+            this.PhoneCode = PhoneCode;
+
+
+            Mode = enMode.Update;
+        }
+
+
+        private  bool _AddNewCountry()
+        {
+            this.CountryID = clsContactDataAccess.AddNewCountry(CountryName , Code , PhoneCode);
+
+            return this.CountryID != -1;
+        }
+
+        private bool _UpdateCountry()
+        {
+            return clsContactDataAccess.UpdateCountry(this.CountryID, this.CountryName , this.Code , this.PhoneCode);
+        }
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNewCountry())
+                    {
+
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case enMode.Update:
+                    return _UpdateCountry();
+
+            }
+
+            return false;
+
+        }
+
+        public static clsCountry Find (int ID)
+        {
+            string CountryName = "";
+            string Code = "";
+            string PhoneCode = "";
+
+
+            if (clsContactDataAccess.GetCountryInfoByID( ID,ref CountryName ,ref Code,ref PhoneCode ))
+            {
+                return new clsCountry(ID ,  CountryName ,Code, PhoneCode);
+            }
+            else { return null; }
+
+
+        }
+
+        public static clsCountry Find(string CountryName)
+        {
+            int CountryID = -1;
+            string Code = "";
+            string PhoneCode = "";
+
+
+            if (clsContactDataAccess.GetCountryInfoByName(CountryName, ref CountryID, ref Code, ref PhoneCode))
+            {
+                return new clsCountry(CountryID, CountryName,Code, PhoneCode);
+            }
+            else { return null; }
+
+
+        }
+
+        public static bool DeleteCountry(int CountryID) 
+        { 
+
+            return clsContactDataAccess.DeleteCountry(CountryID);
+
+        }
+
+
+    }
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,224 @@
+﻿
+using System;
+using System.Data;
+using System.Linq;
+using ContactsBusinessLayer;
+
+namespace ContactsConsolApp
+{
+    internal class Program
+    {
+        static void testFindContact(int ID)
+
+        {
+            clsContact Contact1 = clsContact.Find(ID);
+
+            if (Contact1 != null)
+            {
+                Console.WriteLine(Contact1.FirstName + " " + Contact1.LastName);
+                Console.WriteLine(Contact1.Email);
+                Console.WriteLine(Contact1.Phone);
+                Console.WriteLine(Contact1.Address);
+                Console.WriteLine(Contact1.DateOfBirth);
+                Console.WriteLine(Contact1.CountryID);
+                Console.WriteLine(Contact1.ImagePath);
+            }
+            else
+            {
+                Console.WriteLine("Contact [" + ID + "] Not found!");
+            }
+        }
+
+        static void testAddNewContact()
+
+
+        {
+            clsContact Contact1 = new clsContact();
+
+            Contact1.FirstName = "Fadi";
+            Contact1.LastName = "Maher";
+            Contact1.Email = "A@a.com";
+            Contact1.Phone = "010010";
+            Contact1.Address = "address1";
+            Contact1.DateOfBirth = new DateTime(1977, 11, 6, 10, 30, 0);
+            Contact1.CountryID = 1;
+            Contact1.ImagePath = "";
+
+            if (Contact1.Save())
+            {
+
+                Console.WriteLine("Contact Added Successfully with id=" + Contact1.ID);
+            }
+
+        }
+
+        static void testUpdateContact(int ID)
+
+        {
+            clsContact Contact1 = clsContact.Find(ID);
+
+            if (Contact1 != null)
+            {
+                //update whatever info you want
+                Contact1.FirstName = "Lina";
+                Contact1.LastName = "Maher";
+                Contact1.Email = "A2@a.com";
+                Contact1.Phone = "2222";
+                Contact1.Address = "222";
+                Contact1.DateOfBirth = new DateTime(1977, 11, 6, 10, 30, 0);
+                Contact1.CountryID = 1;
+                Contact1.ImagePath = "";
+
+                if (Contact1.Save())
+                {
+
+                    Console.WriteLine("Contact updated Successfully ");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Not found!");
+            }
+        }
+
+        static void testDeleteContact(int ID)
+
+        {
+
+            if (clsContact.isContactExist(ID))
+
+                if (clsContact.DeleteContact(ID))
+
+                    Console.WriteLine("Contact Deleted Successfully.");
+                else
+                    Console.WriteLine("Faild to delete contact.");
+
+            else
+                Console.WriteLine("The contact with id = " + ID + " is not found");
+
+        }
+
+        static void ListContacts()
+        {
+
+            DataTable dataTable = clsContact.GetAllContacts();
+
+            Console.WriteLine("Contacts Data:");
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Console.WriteLine($"{row["ContactID"]},  {row["FirstName"]} {row["LastName"]}");
+            }
+
+        }
+        static void testIsContactExist(int ID)
+
+        {
+
+            if (clsContact.isContactExist(ID))
+
+                Console.WriteLine("Yes, Contact is there.");
+
+            else
+                Console.WriteLine("No, Contact Is not there.");
+
+        }
+
+        //==============================================
+
+        static void testFindCountry(int ID)
+        {
+            clsCountry country = clsCountry.Find(ID);
+
+            if (country != null)
+            {
+                Console.WriteLine("\nCountry ID: " + ID);
+                Console.WriteLine("\nCountry Name: " + country.CountryName);
+            }
+
+        }
+        static void testFindCountry(string CountryName)
+        {
+            clsCountry country = clsCountry.Find(CountryName);
+
+            if (country != null)
+            {
+                Console.WriteLine("\nCountry ID: " + country.CountryID);
+                Console.WriteLine("\nCountry Name: " + country.CountryName);
+            }
+
+        }
+
+        static void testAddNewCountry(string CountryName, string Code = "" ,string PhoneCode = "")
+        {
+            clsCountry country = new clsCountry();
+            country.CountryName = CountryName;
+            country.Code = Code;
+            country.PhoneCode = PhoneCode;
+
+            if (country.Save())
+            {
+                Console.WriteLine("Saved Successfuly");
+            }
+
+        }
+
+        static void testUpdateCountry(int ID , string CountryName)
+        {
+            clsCountry country = clsCountry.Find(ID);
+
+            
+            country.CountryName = CountryName ;
+
+            if (country.Save())
+            {
+                Console.WriteLine("Updated Successfuly");
+            }
+            else
+            {
+                Console.WriteLine("Faild");
+            }
+        }
+
+        static void testDeleteCountry(int ID)
+        {
+            if (clsCountry.DeleteCountry(ID))
+            {
+                Console.WriteLine("Deleted Successfuly");
+            }
+            else   
+                Console.WriteLine("Faild");
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            // testFindContact(6);
+
+            // testAddNewContact();
+
+
+            //  testUpdateContact(1);
+
+            // testDeleteContact(100);
+
+            //  ListContacts();
+
+            //testIsContactExist(4);
+
+            //testFindCountry("United States");
+
+            testAddNewCountry("Jordan");
+
+            //testUpdateCountry(7, "Jordan");
+
+            //testDeleteCountry(7);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
